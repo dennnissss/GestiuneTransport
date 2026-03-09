692ddd24c9ef44e5453227ce4c6c6ce2f0ea5df2
mannd <mannd@GRASU>
2026-03-10 00:36:19 +0200

    Add project files.

---
+++ tema2/Models/Masina.cs
@@ -0,0 +1,21 @@
+﻿namespace GestiuneTransport.Models
+{
+    public class Masina
+    {
+        public string NrInmatriculare { get; set; }
+        public string Model { get; set; }
+        public double Kilometraj { get; set; }
+
+        public Masina(string nrInmatriculare, string model, double kilometrajInitial)
+        {
+            NrInmatriculare = nrInmatriculare;
+            Model = model;
+            Kilometraj = kilometrajInitial;
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} [{NrInmatriculare}] - Odometru: {Kilometraj} km";
+        }
+    }
+}
---
+++ tema2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GestiuneTransport.Models;
+
+class Program
+{
+    static void Main()
+    {
+        // Creare entități
+        Sofer sofer1 = new Sofer(1, "Ion Popescu");
+        Masina masina1 = new Masina("SV-01-ABC", "Volvo FH", 120500);
+
+        // Simulare activitate
+        Console.WriteLine($"Inregistram traseu pentru {sofer1.Nume}...");
+        sofer1.AdaugaTraseu("Suceava - Bucuresti", 450);
+        masina1.Kilometraj += 450;
+
+        // Afisare rezultate
+        Console.WriteLine("\n--- Raport Activitate ---");
+        Console.WriteLine($"Sofer: {sofer1.Nume} | Total KM: {sofer1.TotalKilometriParcursi}");
+        Console.WriteLine($"Trasee efectuate: {string.Join(", ", sofer1.IstoricTrasee)}");
+        Console.WriteLine($"Stare masina: {masina1.ToString()}");
+    }
+}
